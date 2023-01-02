339ee0ca0ad6fb0188b52c5d2884536db1c62193
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-02 18:12:34 +0700

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,6 @@
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake db:migrate
