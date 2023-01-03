d27cb871f54dd9a30400a26fde35f5e52d54e2c2
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-04 00:23:22 +0700

    Add uniqueness to game community title

--- app/models/game_community.rb
+++ app/models/game_community.rb
@@ -2,7 +2,7 @@ class GameCommunity < ApplicationRecord
     has_many :followings
     has_many :users, through: :followings
 
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: { case_sensitive: false }
     validates :image_url, presence: true
     validates :icon_url, presence: true
 end
