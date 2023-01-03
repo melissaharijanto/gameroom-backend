1f59bc74d02cb74ecb74d37167957e278a1f3a42
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-03 17:41:30 +0700

    Add JsonWebToken class

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+
 end
